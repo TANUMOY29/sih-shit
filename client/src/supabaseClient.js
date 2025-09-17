da718371b522d82560bc08940bb01ae88728ce1c
TANUMOY <ece2024022@rcciit.org.in>
2025-09-17 14:12:16 +0530

    seperate env file

--- client/src/supabaseClient.js
+++ client/src/supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xfznswfpyqafxceppfoz.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhmem5zd2ZweXFhZnhjZXBwZm96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NTM3NDgsImV4cCI6MjA3MzQyOTc0OH0.Lkl8gtRxJh1No5DnQyVu0V81V8S7-FrnjkHtkb-iCcw';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
